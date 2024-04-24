cb6a6273ab1a5928c39736015a3ca23a8f70e66c
charmingQin <1148851466@qq.com>
2024-04-24 14:15:15 +0800

    Update build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -1,10 +1,9 @@
-git clone https://github.com/llvm/llvm-project.git
-cd llvm-project
-git checkout llvmorg-13.0.1
-cd ..
+rm -rf llvm-project
+git clone -b llvmorg-14.0.0 --single-branch --depth=1 https://github.com/llvm/llvm-project.git
+rm -rf llvm-build
 mkdir llvm-build
 cd llvm-build/
-CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../llvm-release -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt" -G "Unix Makefiles" ../llvm-project/llvm
+CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../llvm-release -DLLVM_TARGET_ARCH=arm -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=ARM -DLLVM_TARGETS_TO_BUILD=ARM -DLLVM_DEFAULT_TARGET_TRIPLE=aarch64-apple-darwin -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;compiler-rt" -G "Unix Makefiles" ../llvm-project/llvm
 make -j4
 make install
 cd ..
